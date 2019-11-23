98572c89374ae48bcd4c3d499e5e7b030afc2321
Glitch (ta-app) <none>
2019-11-23 23:41:33 +0000

    🍩🌮 Checkpoint ./.env.development:8815149/138 ./src/components/login/Login.tsx:8815149/938 ./package.json:5140092/72 ./src/pages/LoginPage.tsx:5140092/343

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,45 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import { Form, Button, Container, Row } from 'react-bootstrap';
 
-interface LoginProps {
-  
-}
 
-interface LoginState {
-  
-}
+function Login() {
+  const [validated, setValidated] = useState(false);
 
-class Login extends Component<LoginProps, LoginState> {
-  constructor(props: LoginProps) {
-    super(props);
-    this.state = {
-      username: '',
-      password: ''
+  const handleSubmit = event => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
     }
-  }
+
+    setValidated(true);
+  };  
   
-  render() {
-    return (
-      <div>
-        <Container>
-          <Row className="justify-content-md-center">
-            <Form>
-              <Form.Label>Login</Form.Label>
-              <Form.Group>
-                <Form.Control type="email" placeholder="Email"></Form.Control>
-              </Form.Group>
-              <Form.Group>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Login
-              </Button>
-            </Form>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Form>
+            <Form.Label>Login</Form.Label>
+            <Form.Group>
+              <Form.Control type="email" placeholder="Email"></Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </Form>
+        </Row>
+      </Container>
+    </div>
+  );
+  
 }
 
 export default Login;
