42e39f6829275b7e5efd0f604c50d3939a3dec21
Glitch (ta-app) <none>
2019-11-23 22:45:22 +0000

    👁🌛 Checkpoint ./src/components/header/Header.tsx:8815149/214 ./public/index.html:8815149/733 ./src/components/login/Login.tsx:8815149/351

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
 
 function Header() {
   return (
     <Container>
-      <Navbar expand="lg" variant="light" bg="light">
+      <Navbar expand="lg" variant="light" bg="dark">
         <Navbar.Brand href="#">Navbar</Navbar.Brand>
       </Navbar>
     </Container>
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Login() {
-  return (<div>..</div>);
+
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+  
+  render() {
+    return (
+      <>
+        <div>
+        </div>
+      <>
+    )
+  }
 }
 
 export default Login;
