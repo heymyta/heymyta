588049f751c59c2f319054166967bd43001ec3fb
Glitch (ta-app) <none>
2019-11-24 06:33:52 +0000

    Before import of HuyNVuong/Ta-App without specified path

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {
+  Route, 
+  Switch, BrowserRouter
+} from 'react-router-dom';
+
+import LoginPage from './pages/LoginPage';
+import CoursesPage from './pages/CoursesPage';
+import CoursePage from './pages/CoursePage';
+
+
+class App extends Component {
+  render() {
+    const App = () => (
+      <div>
+        <Route exact path='/' component={LoginPage}/>      
+        <Route exact path='/courses/' component={CoursesPage}/>
+        <Route path='/courses/:courseId' component={CoursePage} />
+      </div>
+    );
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <App/>
+          </Switch>
+        </BrowserRouter>
+      </div>  
+    );
+  }
+}
+
+export default App;
---
+++ src/components/courses/StudentCards.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function StudentCards() {
+  
+}
+
+export default StudentCards;
