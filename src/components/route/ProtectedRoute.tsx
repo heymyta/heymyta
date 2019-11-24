6a9b9c496ca8e4ab9cd132f388f69c9f55d13a43
Glitch (ta-app) <none>
2019-11-24 23:18:35 +0000

    👩‍👩‍👦👌 Checkpoint ./src/components/route/ProtectedRoute.tsx:5140092/343 ./src/components/route/Redirecting.tsx:5140092/218

--- src/components/route/ProtectedRoute.tsx
+++ src/components/route/ProtectedRoute.tsx
@@ -3,6 +3,7 @@ import {
   Redirect,
   Route
 } from 'react-router-dom';
+import Redirecting from './Redirecting';
 
 const ProtectedRoute = ({component: Component, ...props}) => {
   let [isAuth, setIsAuth] = useState();
@@ -12,16 +13,17 @@ const ProtectedRoute = ({component: Component, ...props}) => {
   useEffect(() => {
       props.auth.whoami().then((res) => {
         console.log('after whoami', res);
+        console.log('set', props.auth.logedIn);
         setIsAuth(props.auth.logedIn);
         console.log('isAuth', isAuth);
       });
   });
-  
+  console.log('before render isAuth', isAuth);
   return (
     <Route {...props} render={(props) => (
       isAuth == true 
         ? <Component {...props} />
-        : <Redirect to='/' />
+        : <Redirecting />
     )} />
   );
 }
