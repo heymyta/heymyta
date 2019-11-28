2c4c86a313eb736ebbf6f4fc1e1f9dc1dd6fd61d
Glitch (ta-app) <none>
2019-11-28 16:02:52 +0000

    🚧🌎 Checkpoint ./src/components/footer/index.tsx:5140092/656

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,8 +3,6 @@ import {
   Container, Modal,
   Button
 } from 'react-bootstrap';
-
-import ContactUsModal from '../contactUsModal/';
 interface MyProps {
 }
 
@@ -12,29 +10,10 @@ function Footer(props: MyProps) {
   const [modalShow, setModalShow] = useState(false);
 
   return (
-    /*<div style={{bottom: 0, position: 'fixed', width: '100%'}}>
-      <Container className="bg-dark">
-        <Button onClick={() => setModalShow(true)}>
-          Contact us
-        </Button>
-      </Container>
-
-      
-      <ContactUsModal show={modalShow} onHide={() => setModalShow(false)} />
-    </div>*/
     <div>
-      <hr className="featurette-divider" />
-      
       <footer className="text-muted" >
 
-        <Container fluid>
-          <Button variant="link" onClick={() => setModalShow(true)}>
-            Contact us
-          </Button>
-        </Container>
       </footer>
-      
-      <ContactUsModal show={modalShow} onHide={() => setModalShow(false)} />
     </div>
   );
 }
